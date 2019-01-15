774bda71b1bf2e9d7bc01f36cdac3b5eaa0375f9
杨自超 <2391985478@qq.com>
2019-01-15 20:09:45 +0800

    tianjiadiyigegongneng

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
-int main(){
+char arr[1024];
+int index=0;
+int main()
+{
 
     printf("xitong\n");
 
-    while(1){
+    while (1)
+    {
         printf("1---tianjia\n");
         printf("2---shanchu\n");
         printf("3---chakan\n");
@@ -13,32 +16,39 @@ int main(){
 
         printf("gongneng\n");
 
-        int code;
-        scanf("%d",&code);
+        int code = 0;
+        scanf("%d", &code);
 
-        if(code == 1){
+        if (code == 1)
+        {
+            printf("请输入要添加的敏感字符\n");
+            char m;
+            scanf("%c", &m); //这一个scanf是把上一次的回车字符从电脑中取出
+            scanf("%c", &m); //这一行scanf才是真正的取出用户输入的敏感字符
 
+            arr[index] = m;
+            index++;
 
+            printf("添加成功，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if(code == 2){
-
-            
+        if (code == 2)
+        {
         }
-        if(code == 3){
-
-            
+        if (code == 3)
+        {
         }
-        if(code == 4){
-
-            
+        if (code == 4)
+        {
         }
-        if(code == 5){
+        if (code == 5)
+        {
             printf("tuichu\n");
             break;
-            
         }
     }
 
     return 0;
-
 }
