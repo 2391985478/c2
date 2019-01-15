cf22caeb65b4ba788b45fe4a5a7f4a4528bfdd89
杨自超 <2391985478@qq.com>
2019-01-15 20:17:54 +0800

    tianjiaxianshigongneng

--- main.c
+++ main.c
@@ -57,6 +57,20 @@ int main()
         }
         if (code == 3)
         {
+            printf("你的敏感字符为：\n");
+            for(int i=0;i<index;i++){
+                printf("你的第%d个敏感字符为%c\n",i+1,arr[i]);
+            for (int i = 0; i < index; i++)
+            {
+                printf("你的第%d个敏感字符为%c\n", i + 1, arr[i]);
+            }
+
+            printf("点击回车继续\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 4)
         {
